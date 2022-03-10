419b63090c5c26e2920c9fbd892ef388ac76e13e
Stephen Jakku <uttamjakku96@gmail.com>
2022-03-09 16:39:26 -0800

    test

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar({ executeScroll, comp }) {
   const [isOpen, setOpen] = useState(false);
   return (
     <div className="grid grid-cols-2 h-12 bg-black">
-      <div className="flex text-white justify-start items-center text-xl tracking-wide">
+      <div className="flex text-white justify -start items-center text-xl tracking-wide">
         <h1 className="pl-4 hover:text-3xl cursor-default text-2xl font-medium">
           SJ
         </h1>
@@ -17,8 +17,8 @@ function Navbar({ executeScroll, comp }) {
         <ul className="flex items-center text-white text-lg space-x-4 mr-4">
           <li
             onClick={() => {
-              executeScroll();
               comp("abt");
+              executeScroll();
             }}
             className="hover:border-b-2 hover:border-red-600 p-1 cursor-default "
           >
@@ -26,8 +26,8 @@ function Navbar({ executeScroll, comp }) {
           </li>
           <li
             onClick={() => {
-              executeScroll();
               comp("exp");
+              executeScroll();
             }}
             className="hover:border-b-2 hover:border-cyan-400 p-1  cursor-default"
           >
@@ -35,8 +35,8 @@ function Navbar({ executeScroll, comp }) {
           </li>
           <li
             onClick={() => {
-              executeScroll();
               comp("prj");
+              executeScroll();
             }}
             className="hover:border-b-2 hover:border-orange-400 p-1  cursor-default"
           >
